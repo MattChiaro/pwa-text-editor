d343b8792733991b4f4ee512108a80515e97ea6a
Matt Chiaro <131219830+MattChiaro@users.noreply.github.com>
2023-07-18 11:26:47 -0500

    removed unused plugin

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -22,7 +22,6 @@ module.exports = () => {
       new HtmlWebpackPlugin({
         template: './index.html'
       }),
-      // new GenerateSW(),
       new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
